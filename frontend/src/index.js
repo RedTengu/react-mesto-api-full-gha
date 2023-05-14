f521968fef9f82be690f26a29a5d57e47fe0fa7b
RedTengu <muratborokov@gmail.com>
2023-05-14 20:45:12 +0300

    refactor: поменять Hrouter на Brouter.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 
 import './index.css';
 
@@ -8,7 +8,7 @@ import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
